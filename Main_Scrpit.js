819d59f47fa7eefb909158d46e32d4741f9469c4
Someshsb12 <132648875+Someshsb12@users.noreply.github.com>
2023-10-25 12:44:35 +0200

    Add files via upload

---
+++ Main_Scrpit.js
@@ -0,0 +1,243 @@
+
+    function showMap_1() {
+      var Dropdown = document.getElementById("school1-route");
+      var selected = Dropdown.value;
+      
+      var RouteFiles = {
+        "Route 1": "01-PIS/Route-01/index.html",
+        "Route 2": "01-PIS/Route-02/index.html",
+        "Route 3": "01-PIS/Route-03/index.html",
+        "Route 4": "01-PIS/Route-04/index.html",
+      };
+      if (selected in RouteFiles) {
+        var mapFile = RouteFiles[selected];
+        window.open(mapFile,"_blank");
+      }}
+
+    function showMap_2() {
+        var Dropdown = document.getElementById("school2-route");
+        var selected = Dropdown.value;
+        
+        var RouteFiles = {
+          "Route 1": "02-DAYS/Route-01/index.html",
+          "Route 2": "02-DAYS/Route-02/index.html",
+          "Route 3": "02-DAYS/Route-03/index.html",
+        };
+        if (selected in RouteFiles) {
+          var mapFile = RouteFiles[selected];
+          window.open(mapFile,"_blank");
+        }}
+
+    function showMap_3() {
+          var Dropdown = document.getElementById("school3-route");
+          var selected = Dropdown.value;
+          
+          var RouteFiles = {
+            "Route 1": "03-MSM/Route-01/index.html",
+            "Route 2": "03-MSM/Route-01/index.html",
+            "Route 2": "03-MSM/Route-01/index.html",
+          };
+          if (selected in RouteFiles) {
+            var mapFile = RouteFiles[selected];
+            window.open(mapFile,"_blank");
+          }}
+
+      function showMap_4() {
+        var Dropdown = document.getElementById("school4-route");
+          var selected = Dropdown.value;
+            
+            var RouteFiles = {
+              "Route 1": "04-RCIS/Route-01/index.html",
+              "Route 2": "04-RCIS/Route-02/index.html",
+              "Route 3": "04-RCIS/Route-03/index.html",
+            };
+            if (selected in RouteFiles) {
+              var mapFile = RouteFiles[selected];
+              window.open(mapFile,"_blank");
+            }}  
+
+      function showMap_5() {
+        var Dropdown = document.getElementById("school5-route");
+              var selected = Dropdown.value;
+              
+              var RouteFiles = {
+                "Route 1": "05-MPSM/Route-01/index.html",
+                "Route 2": "05-MPSM/Route-02/index.html",
+              //   "Route 3": "NHG/NHGR_Route_03/index.html",
+              };
+              if (selected in RouteFiles) {
+                var mapFile = RouteFiles[selected];
+                window.open(mapFile,"_blank");
+              }}      
+
+      function showMap_6() {
+        var Dropdown = document.getElementById("school6-route");
+        var selected = Dropdown.value;
+        
+        var RouteFiles = {
+          "Route 1": "NHVM/NHVMR_Route_01/index.html",
+          "Route 2": "NHVM/NHVMR_Route_02/index.html",
+          "Route 3": "NHVM/NHVMR_Route_03/index.html",
+        };
+        if (selected in RouteFiles) {
+          var mapFile = RouteFiles[selected];
+          window.open(mapFile,"_blank");
+        }}      
+
+      function showMap_7() {
+        var Dropdown = document.getElementById("school7-route");
+        var selected = Dropdown.value;
+        
+        var RouteFiles = {
+          "Route 1": "NHVM/NHVMR_Route_01/index.html",
+          "Route 2": "NHVM/NHVMR_Route_02/index.html",
+          "Route 3": "NHVM/NHVMR_Route_03/index.html",
+        };
+        if (selected in RouteFiles) {
+          var mapFile = RouteFiles[selected];
+          window.open(mapFile,"_blank");
+        }}       
+
+       function showMap_8() {
+        var Dropdown = document.getElementById("school8-route");
+        var selected = Dropdown.value;
+        
+        var RouteFiles = {
+          "Route 1": "NHVM/NHVMR_Route_01/index.html",
+          "Route 2": "NHVM/NHVMR_Route_02/index.html",
+          "Route 3": "NHVM/NHVMR_Route_03/index.html",
+        };
+        if (selected in RouteFiles) {
+          var mapFile = RouteFiles[selected];
+          window.open(mapFile,"_blank");
+        }}        
+
+       function showMap_9() {
+        var Dropdown = document.getElementById("school9-route");
+        var selected = Dropdown.value;
+        
+        var RouteFiles = {
+          "Route 1": "NHVM/NHVMR_Route_01/index.html",
+          "Route 2": "NHVM/NHVMR_Route_02/index.html",
+          "Route 3": "NHVM/NHVMR_Route_03/index.html",
+        };
+        if (selected in RouteFiles) {
+          var mapFile = RouteFiles[selected];
+          window.open(mapFile,"_blank");
+        }}            
+
+       function showMap_10() {
+        var Dropdown = document.getElementById("school10-route");
+        var selected = Dropdown.value;
+        
+        var RouteFiles = {
+          "Route 1": "NHVM/NHVMR_Route_01/index.html",
+          "Route 2": "NHVM/NHVMR_Route_02/index.html",
+          "Route 3": "NHVM/NHVMR_Route_03/index.html",
+        };
+        if (selected in RouteFiles) {
+          var mapFile = RouteFiles[selected];
+          window.open(mapFile,"_blank");
+        }}              
+
+        function showMap_11() {
+          var Dropdown = document.getElementById("school11-route");
+          var selected = Dropdown.value;
+          
+          var RouteFiles = {
+            "Route 1": "NHVM/NHVMR_Route_01/index.html",
+            "Route 2": "NHVM/NHVMR_Route_02/index.html",
+            "Route 3": "NHVM/NHVMR_Route_03/index.html",
+          };
+          if (selected in RouteFiles) {
+            var mapFile = RouteFiles[selected];
+            window.open(mapFile,"_blank");
+          }}                 
+
+        function showMap_12() {
+          var Dropdown = document.getElementById("school12-route");
+          var selected = Dropdown.value;
+          
+          var RouteFiles = {
+            "Route 1": "NHVM/NHVMR_Route_01/index.html",
+            "Route 2": "NHVM/NHVMR_Route_02/index.html",
+            "Route 3": "NHVM/NHVMR_Route_03/index.html",
+          };
+          if (selected in RouteFiles) {
+            var mapFile = RouteFiles[selected];
+            window.open(mapFile,"_blank");
+          }} 
+
+        function showMap_13() {
+            var Dropdown = document.getElementById("school13-route");
+            var selected = Dropdown.value;
+            
+            var RouteFiles = {
+              "Route 1": "NHVM/NHVMR_Route_01/index.html",
+              "Route 2": "NHVM/NHVMR_Route_02/index.html",
+              "Route 3": "NHVM/NHVMR_Route_03/index.html",
+            };
+            if (selected in RouteFiles) {
+              var mapFile = RouteFiles[selected];
+              window.open(mapFile,"_blank");
+            }}   
+            
+        function showMap_14() {
+              var Dropdown = document.getElementById("school14-route");
+              var selected = Dropdown.value;
+              
+              var RouteFiles = {
+                "Route 1": "NHVM/NHVMR_Route_01/index.html",
+                "Route 2": "NHVM/NHVMR_Route_02/index.html",
+                "Route 3": "NHVM/NHVMR_Route_03/index.html",
+              };
+              if (selected in RouteFiles) {
+                var mapFile = RouteFiles[selected];
+                window.open(mapFile,"_blank");
+              }} 
+
+         function showMap_15() {
+                var Dropdown = document.getElementById("school15-route");
+                var selected = Dropdown.value;
+                
+                var RouteFiles = {
+                  "Route 1": "NHVM/NHVMR_Route_01/index.html",
+                  "Route 2": "NHVM/NHVMR_Route_02/index.html",
+                  "Route 3": "NHVM/NHVMR_Route_03/index.html",
+                };
+                if (selected in RouteFiles) {
+                  var mapFile = RouteFiles[selected];
+                  window.open(mapFile,"_blank");
+                }} 
+
+         function showMap_16() {
+                  var Dropdown = document.getElementById("school16-route");
+                  var selected = Dropdown.value;
+                  
+                  var RouteFiles = {
+                    "Route 1": "NHVM/NHVMR_Route_01/index.html",
+                    "Route 2": "NHVM/NHVMR_Route_02/index.html",
+                    "Route 3": "NHVM/NHVMR_Route_03/index.html",
+                  };
+                  if (selected in RouteFiles) {
+                    var mapFile = RouteFiles[selected];
+                    window.open(mapFile,"_blank");
+                  }} 
+
+          function showMap_17() {
+                    var Dropdown = document.getElementById("school17-route");
+                    var selected = Dropdown.value;
+                    
+                    var RouteFiles = {
+                      "Route 1": "NHVM/NHVMR_Route_01/index.html",
+                      "Route 2": "NHVM/NHVMR_Route_02/index.html",
+                      "Route 3": "NHVM/NHVMR_Route_03/index.html",
+                    };
+                    if (selected in RouteFiles) {
+                      var mapFile = RouteFiles[selected];
+                      window.open(mapFile,"_blank");
+                    }}          
+        
+              
+
+

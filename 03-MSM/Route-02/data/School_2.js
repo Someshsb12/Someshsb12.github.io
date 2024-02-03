5e271ca613091d5cccbcff57b0ced54e5ba580c7
Someshsb12 <132648875+Someshsb12@users.noreply.github.com>
2024-02-03 05:35:53 -1200

    Add files via upload

---
+++ 03-MSM/Route-02/data/School_2.js
@@ -0,0 +1,1 @@
+var json_School_2 = {"type":"FeatureCollection","name":"School_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Name":"Mahabodhi School, Mysuru"},"geometry":{"type":"Point","coordinates":[76.630038091255869,12.305223798736991]}}]}

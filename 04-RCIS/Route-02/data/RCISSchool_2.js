4dcf4c9ce22f4921331a46b0085f657a0ca6a44b
Someshsb12 <132648875+Someshsb12@users.noreply.github.com>
2024-02-03 06:11:19 -1200

    Add files via upload

---
+++ 04-RCIS/Route-02/data/RCISSchool_2.js
@@ -0,0 +1,1 @@
+var json_RCISSchool_2 = {"type":"FeatureCollection","name":"RCISSchool_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"School Nam":"Royale Concorde International School, Mysore"},"geometry":{"type":"Point","coordinates":[76.606670516261445,12.310993889733354]}}]}

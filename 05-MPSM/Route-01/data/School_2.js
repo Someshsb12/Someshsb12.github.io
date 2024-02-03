89d3d086063f2d3ef002932639ecab99cc8aba34
Someshsb12 <132648875+Someshsb12@users.noreply.github.com>
2024-02-03 06:26:26 -1200

    Add files via upload

---
+++ 05-MPSM/Route-01/data/School_2.js
@@ -0,0 +1,1 @@
+var json_School_2 = {"type":"FeatureCollection","name":"School_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Name":"Maharshi Public School"},"geometry":{"type":"Point","coordinates":[76.645091506954373,12.272413674122806]}}]}
